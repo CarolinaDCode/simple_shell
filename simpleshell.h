7ab3d5c318308320ed50e500c96ceedc8d24d9b3
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-08-12 14:05:28 -0700

    README - man - AUTHORS

---
+++ simpleshell.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLESHELL_H
+#define SIMPLESHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#endif
